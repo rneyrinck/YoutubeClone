ec09df83e5f56e5eb30c41e3d7df258c36d30239
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-15 15:53:21 -0500

    added fetch request for videos, connected feed and videos component and tested fetch functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import SideBar from "./SideBar";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromApis";
+
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,15 +22,29 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <SideBar />
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
-          classname="copyright"
+          className="copyright"
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
           CopyRight 2022 Robert Neyrinck
         </Typography>
-       
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory}
+          <span style={{ color: "#Fc1503" }}> videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
