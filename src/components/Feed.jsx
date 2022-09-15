2d01e84d22d6cae06b7d6b3c3c45fae37f43adc8
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-15 15:10:28 -0500

    added sidebar with icons, hover and filler click functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
 const Feed = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          classname="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          CopyRight 2022 Robert Neyrinck
+        </Typography>
+       
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
