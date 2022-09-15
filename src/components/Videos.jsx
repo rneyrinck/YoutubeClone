d6040d9e465da2289776bf92bc77812da3e91ea5
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-15 16:05:31 -0500

    fleshed out videoCard data. now showing thumbnail for each video in feed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,8 +6,8 @@ const Videos = ({ videos }) => {
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
         {videos.map((item, idx)=> (
             <Box key={idx}>
-                {item.id.videoID && <VideoCard video={item}/>}
-                {item.id.channelID && <ChannelCard channelDetail={item}/>}
+                {item.id.videoId && <VideoCard video={item}/>}
+                {/* {item.id.channelId && <ChannelCard channelDetail={item}/>} */}
             </Box>
         ))}
     </Stack>
