13c5a882c5f3c122410d011d8edd3e3e8b3aa7d8
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-20 16:47:46 -0500

    fleshing out channel detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { VideoSettings } from "@mui/icons-material";
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromApis";
+import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
-  return (
-    <div>
-      channeldetail
-    </div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
-export default ChannelDetail
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
+  return <div>channeldetail</div>;
+};
+
+export default ChannelDetail;
