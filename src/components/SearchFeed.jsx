f671a70ee8352842112e812309ac1c6d16bd4735
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-20 17:13:00 -0500

    added searchbar functionality, tested and confirmed usage is correct

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -18,8 +18,8 @@ const Feed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search Results for: 
-        <span style={{ color: "#F31503" }}>{searchTerm}</span>
+        Search Results for:  
+        <span style={{ color: "#F31503" }}> {searchTerm}</span>
       </Typography>
       <Videos videos={videos} />
     </Box>
