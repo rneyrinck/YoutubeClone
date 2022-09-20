7aebcb50e2855569f1797a69a7b1d6dad6d8a79f
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-20 16:03:55 -0500

    added video card title and channel, linked them up and it works for larger breakpoints

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 const VideoCard = ({video: { id: {videoId}, snippet}}) => {
   return (
-    <Card>
+    <Card sx={{width: {md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 'none'}}>
       <Link to={videoId ? `/video/${videoId}`: demoVideoUrl}>
       <CardMedia 
       image={snippet?.thumbnails?.high?.url}
@@ -15,6 +15,19 @@ const VideoCard = ({video: { id: {videoId}, snippet}}) => {
       sx={{width: 358, height: 180}}
       />
       </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}}>
+        <Link to={videoId ? `/video/${videoId}`: demoVideoUrl}>
+          <Typography variant='subtitle1' fontweight="bold" color='#FFF'>
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0,60)}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' fontweight="bold" color='#808080'>
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0,60)}
+            <CheckCircle sx={{fontSize: 12, color: '#808080', ml: 5}} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
